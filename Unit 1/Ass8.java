929d494650c62928341a5539a841c5a9afca5d80
rutik <kalerutik656@gmail.com>
2023-09-20 13:48:06 +0530

    updating Ass8

--- Unit 1/Ass8.java
+++ Unit 1/Ass8.java
@@ -2,8 +2,24 @@ import java.util.Scanner;
 import Games.*;
 
 public class Ass8{
+	Scanner sc = new Scanner(System.in);
+
+	public void inputRecords(int noStudent){
+		
+	}
+
 	public static void main(String arg[]){
-		Scanner sc = new Scanner(System.in);
+		int noStudent;
+		String name;
+		System.out.println("Enter Number of Student : ");
+		noStudent = sc.nextInt();
+		Indoor obj[] = new Indoor[noStudent];
+		for(int i=0;i<noStudent;i++){
+			System.out.println("Enter Student Name : ");
+			name = sc.nextInt();
+			obj[i] = new Indoor(name);
+		}
+		
 		
 	}
 }
